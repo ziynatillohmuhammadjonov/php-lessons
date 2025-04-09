66e1ac82223ca97cb991854430d41c717d1d2e74
Ziynatilloh <zmozugrand@gmail.com>
2025-04-09 13:00:46 +0500

    Laravel 1 lesson

--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group([], function(){
+    Route::get('/users', function () {
+        return view('welcome');
+    });
+
+    Route::get('/employers', function () {
+        return view('welcome');
+    });
+});
